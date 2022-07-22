2c82df49a594f11af3428b1053f731ea84b18a95
Mike Shippy <mikeship04@gmail.com>
2022-07-22 08:31:59 -0700

    commit tree ab493ee615060f8bbb3a0b7da683feea1d4eaf85

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def line(array)
+   if array.empty?
+     puts "The line is currently empty."
+   else
+    current_line = "The line is currently:"
+    array.map.with_index(1) do |person, i|
+        current_line << " #{i}. #{person}"
+    end
+    puts current_line
+   end
+end
+
+def take_a_number(array, string)
+    array << string
+    puts "Welcome, #{string}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+    if array.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array.first}."
+        array.shift
+    end
+end
